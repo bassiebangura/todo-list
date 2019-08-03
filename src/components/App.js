d475bede68a276d5af7810021bf5223d6d3d4d1d
bassiebangura <devbassiebangura@gmail.com>
2019-08-03 17:20:49 -0400

    Add components folder.

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <div>
+     <h1>Hello To My todo-list App</h1>
+   </div>
+  );
+}
+
+export default App;
