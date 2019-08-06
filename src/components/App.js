33d41a7477502d90750c3db6ebfc6f80d9fc96e4
bassiebangura <devbassiebangura@gmail.com>
2019-08-06 07:31:08 -0400

    Add style to element based on task completion.

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "./TodoListItem";
 import TodoListItem from "./TodoListItem";
 import todosData from "../todosData";
@@ -11,29 +11,33 @@ class App extends Component {
     this.state = {
       todos: todosData
     };
-    
   }
   handleChange(id) {
-    
-   this.setState(prevState => {
-       //modify the array in the todos property of the this.state object
-       //console.log(prevState.todos)
-       let updatedTodosData = prevState.todos.map(item => {
-         console.log(item)
-           if(item.id === id) {
-               item.completed = !item.completed;
-               return item
-           } else {
-               return item;
-           }
-       })
-       return {todos: updatedTodosData}
-   })
+    this.setState(prevState => {
+      //modify the array in the todos property of the this.state object
+      //console.log(prevState.todos)
+      let updatedTodosData = prevState.todos.map(item => {
+        console.log(item);
+        if (item.id === id) {
+          item.completed = !item.completed;
+          return item;
+        } else {
+          return item;
+        }
+      });
+      return { todos: updatedTodosData };
+    });
   }
   render() {
     let todosItem = this.state.todos.map(item => (
-      <TodoListItem key={item.id} id={item.id} text={item.text} completed={item.completed} handleChange = {this.handleChange}/>
-    ))
+      <TodoListItem
+        key={item.id}
+        id={item.id}
+        text={item.text}
+        completed={item.completed}
+        handleChange={this.handleChange}
+      />
+    ));
     return (
       <div className="todoList">
         {todosItem} {/*using es6 spread*/}
--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -2,10 +2,15 @@ import React from "react";
 
 
 function TodoListItem({id, text, completed, handleChange}) {
+    let completedStyle = {
+        fontStyle: 'italic',
+        textDecoration: 'line-through'
+        
+    }
   return (
     <div className="todoList__item">
       <input type="checkbox" checked={completed} onChange={() => handleChange(id)}/>
-      <p>{text}</p>
+      <p  style={completed ? completedStyle : null}>{text}</p>
     </div>
   );
 }
